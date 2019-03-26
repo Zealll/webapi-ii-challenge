5d80a404bf967f057108272120e6d5504882f670
unknown <eriznis@gmail.com>
2019-03-25 21:16:09 -0400

    getting an user by id added, but errors arent showing up

--- data/posts-router.js
+++ data/posts-router.js
@@ -17,6 +17,27 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Posts
+    .findById(id)
+    .then(individualPost => {
+        if(!individualPost) {
+            res
+            .status(404)
+            .json({message: `The post with the specified ID of ${id} does not exist.`})
+        } else {
+            res
+            .json(individualPost)
+        }
+    })
+    .catch(err => {
+        res
+        .status(500)
+        .json({error: "The post information could not be retrieved."})
+    })
+})
+
 
 
 
